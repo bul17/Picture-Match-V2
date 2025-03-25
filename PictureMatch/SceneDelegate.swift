7a0f8e5a02b0fbd8888dbe2fc19258fd18b2c795
bul17 <bul17@users.noreply.github.com>
2025-03-25 22:10:23 +0000

    Test
    Minor Change For Test

--- PictureMatch/SceneDelegate.swift
+++ PictureMatch/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
-        print("SceneDelegate is running Still")
+        print("SceneDelegate is running")
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let mainVC = MainViewController()
